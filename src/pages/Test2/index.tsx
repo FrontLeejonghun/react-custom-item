b6cf5f4f45eb2bce0337e33cfaa2b774491099b8
FrontLeejonghun <leejonghun32425@gmail.com>
2022-02-19 19:53:48 +0900

    Update - checkbox multiple example

---
+++ src/pages/Test2/index.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useEffect, useState } from 'react';
+
+type CheckboxItem = {
+  id: string;
+  name: string;
+  checked: boolean;
+};
+
+const Test2 = () => {
+  const [checkboxArray, setCheckboxArray] = useState<CheckboxItem[]>([
+    {
+      id: 'a123',
+      name: 'test1',
+      checked: false,
+    },
+    {
+      id: 'b123',
+      name: 'test2',
+      checked: false,
+    },
+    {
+      id: 'c123',
+      name: 'test3',
+      checked: false,
+    },
+  ]);
+
+  useEffect(() => {
+    console.table(checkboxArray);
+  }, [checkboxArray]);
+
+  const handelChange = useCallback((e, value: CheckboxItem) => {
+    setCheckboxArray((prev) =>
+      prev.map((item) => (item.id === value.id ? { ...item, checked: e.target.checked } : item)),
+    );
+  }, []);
+
+  return (
+    <div>
+      {checkboxArray.map((value) => (
+        <div key={value.id} style={{ marginBottom: '10px' }}>
+          <label htmlFor={value.id}>{value.name}</label>
+          <input
+            id={value.id}
+            type="checkbox"
+            value={value.name}
+            onChange={(e) => handelChange(e, value)}
+          />
+          <span>{value.checked.toString()}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Test2;
